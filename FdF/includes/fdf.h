69ab1f04a25d80c9ffe6e3fb577af08b73c23976
Zied Harzi <zharzi@student.42angouleme.fr>
2022-09-03 00:26:07 +0200

    03sep22

--- FdF/includes/fdf.h
+++ FdF/includes/fdf.h
@@ -6,16 +6,27 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 13:48:56 by zharzi            #+#    #+#             */
-/*   Updated: 2022/09/01 16:38:21 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/09/02 23:28:31 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+# define WINDOW_WIDTH 1920
+# define WINDOW_HEIGHT 1080
+# define MLX_ERROR 1
 
 # include "mlx.h"
 # include "libft.h"
 # include "ft_printf.h"
+
+typedef struct s_data {
+	void	*img;
+	void	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
 /////////////////////////
 //	CATEGORY
--- FdF/main.c
+++ FdF/main.c
@@ -6,12 +6,82 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 13:40:17 by zharzi            #+#    #+#             */
-/*   Updated: 2022/09/01 19:22:33 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/09/03 00:25:16 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, unsigned int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	main(int ac, char **argv)
+{
+	void			*target;
+	void			*window;
+	t_data			image;
+	int				x;
+	int				y;
+	unsigned int	color;
+
+	(void)ac;
+	(void)argv;
+	x = 10;
+	y = 10;
+	color = 0x00FF0000;
+	target = mlx_init();
+	if (!target)
+		return (MLX_ERROR);
+	window = mlx_new_window(target, WINDOW_WIDTH, WINDOW_HEIGHT, "FDF");
+	if (!window)
+	{
+		free(target);
+		return (MLX_ERROR);
+	}
+	image.img = mlx_new_image(target, WINDOW_WIDTH, WINDOW_HEIGHT);
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, \
+		&image.line_length, &image.endian);
+	while (x <= 200)
+	{
+		my_mlx_pixel_put(&image, x, y, color);
+		//color += 0x000001111;
+		x++;
+	}
+	y++;
+	x = 10;
+	while (y < 200)
+	{
+		my_mlx_pixel_put(&image, x, y, color);
+		//color += 0x000001111;
+		while (x++ < 200)
+		{
+			my_mlx_pixel_put(&image, x, y, color + 0x00007FFF);
+			//color += 0x000001111;
+		}
+		x = 10;
+		my_mlx_pixel_put(&image, x * 20, y, color);
+		//color += 0x000001111;
+		y++;
+	}
+	while (x <= 200)
+	{
+		my_mlx_pixel_put(&image, x, y, color);
+		//color += 0x000001111;
+		x++;
+	}
+	mlx_put_image_to_window(target, window, image.img, 0, 0);
+	mlx_loop(target);
+	mlx_destroy_window(target, window);
+	mlx_destroy_display(target);
+	free(target);
+	return (0);
+}
+/*
 int	main(int ac, char **argv)
 {
 	int		fd;
@@ -36,8 +106,7 @@ int	main(int ac, char **argv)
 	mlx_destroy_display(mlx);
 	close(fd);
 	return (0);
-}
-
+}*/
 /*Votre programme doit afficher une image dans une fenêtre.
 • La gestion de la fenêtre doit rester fluide (changer de fenêtre,
 la réduire, etc.).
