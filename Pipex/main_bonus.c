ae77a39c408678a65825b299ec5c43fa5ec59562
Zied Harzi <zharzi@student.42angouleme.fr>
2022-11-03 23:31:12 +0100

    03nov22

--- Pipex/main_bonus.c
+++ Pipex/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 06:22:33 by zharzi            #+#    #+#             */
-/*   Updated: 2022/11/01 23:31:57 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/11/03 23:30:31 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,26 +39,6 @@ int	ft_such_file(char *filename)
 }
 
 
-char	*ft_setup_limiter(const char *src, int *cursor)
-{
-	size_t	i;
-	size_t	size;
-	char	*dst;
-
-	i = 0;
-	size = ft_strlen(src);
-	dst = ft_calloc(size + 2, sizeof(char));
-	while (src[i] && i < (size))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\n';
-	dst[i + 1] = '\0';
-	*cursor += 1;
-	return (dst);
-}
-
 
 void	ft_close_stdfds(void)
 {
@@ -68,14 +48,17 @@ void	ft_close_stdfds(void)
 }
 
 
-void	ft_print_cmderr(char **validpaths, char **argv)
+void	ft_print_cmderr(char **validpaths, char **argv)////////////////////////////
 {
 	char	**cmd_args;
 	int		i;
 
 	i = 0;
+	if (!ft_strncmp(argv[1], "here_doc", 9))
+		argv = argv + 1;
 	while (validpaths && validpaths[i])
 	{
+		printf("%s\n", validpaths[i]);
 		if (validpaths[i][0] == '\0')
 		{
 			cmd_args = ft_split(argv[i + 2], ' ');
@@ -101,21 +84,15 @@ void	ft_double_fullfree(char **strs1, char **strs2)
 	ft_full_free((void **)strs1);
 	ft_full_free((void **)strs2);
 }
-/*
-void	ft_outfile_to_stdout_b(t_data *data)
-{
-	int	fdof;
 
-	ft_get_next_cmd(data);
-	fdof = open(data->outfile, O_RDWR | O_APPEND | O_CREAT, S_IRUSR | \
-		S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	dup2(fdof, 1);
-	if (!fork())
-		execve(data->validpath, data->cmd, data->env);
-	else
-		waitpid(0, NULL, 0);
+
+
+void	ft_clean_exit(char **validpaths, char **cmd_args)
+{
+	ft_double_fullfree(validpaths, cmd_args);
+	ft_close_stdfds();
+	exit(1);
 }
-*/
 
 void	ft_outfile_to_out(char **paths, int i, char **cmd_a, t_data *data)
 {
@@ -134,11 +111,7 @@ void	ft_outfile_to_out(char **paths, int i, char **cmd_a, t_data *data)
 			if (paths[i] && paths[i][0] != '\0')
 				execve(paths[i], cmd_a, data->env);
 			else
-			{
-				ft_double_fullfree(paths, cmd_a);
-				ft_close_stdfds();
-				exit(1);
-			}
+				ft_clean_exit(paths, cmd_a);
 		}
 	}
 	else
@@ -146,6 +119,30 @@ void	ft_outfile_to_out(char **paths, int i, char **cmd_a, t_data *data)
 	ft_full_free((void **)cmd_a);
 }
 
+void	ft_append_to_outfile(char **paths, int i, char **cmd_a, t_data *data)
+{
+	int		fdof;
+	char	*outfile;
+
+	outfile = data->argv[data->ac - 1];
+	fdof = open(outfile, O_RDWR | O_APPEND | O_CREAT, S_IRUSR | \
+		S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fdof > 0)
+	{
+		dup2(fdof, 1);
+		close(fdof);
+		if (!fork())
+		{
+			if (paths[i] && paths[i][0] != '\0')
+				execve(paths[i], cmd_a, data->env);
+			else
+				ft_clean_exit(paths, cmd_a);
+		}
+	}
+	else
+		perror(outfile);
+	ft_full_free((void **)cmd_a);
+}
 
 int	ft_outfile_permission(char *filename)
 {
@@ -161,7 +158,6 @@ int	ft_outfile_permission(char *filename)
 	return (1);
 }
 
-
 t_data	ft_init_data(int ac, char **argv, char **env)
 {
 	t_data	data;
@@ -171,52 +167,6 @@ t_data	ft_init_data(int ac, char **argv, char **env)
 	data.env = env;
 	return (data);
 }
-/*
-int	ft_here_doc(t_data *data, char *limiter)
-{
-	int		fd;
-	int		stop;
-	char	*buffer;
-
-	stop = 0;
-	buffer = NULL;
-	if (data->infile && access(data->infile, F_OK) == 0)
-		data->infile = ft_get_tmpname(&data->infile);
-	fd = open(data->infile, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | \
-		S_IWGRP | S_IROTH);
-	while (!stop)
-	{
-		buffer = get_next_line(STDIN);
-		if (!ft_strncmp(buffer, limiter, ft_strlen(limiter)))
-			stop++;
-		else if (buffer)
-			write(fd, buffer, ft_strlen(buffer));
-		if (buffer)
-			free(buffer);
-	}
-	buffer = get_next_line(1025);
-	ft_true_free((void **)&limiter);
-	return (fd);
-}
-*/
-/*
-void	ft_infile_to_stdin_b(t_data *data)
-{
-	int		fd;
-	char	*limiter;
-	char	*basename;
-
-	basename = "tmp_pipex";
-	data->infile = ft_strdup(basename);
-	limiter = ft_setup_limiter(data->argv[1], &data->cursor);
-	fd = ft_here_doc(data, limiter);
-	close(fd);
-	fd = open(data->infile, O_RDONLY);
-	dup2(fd, STDIN);
-	close(fd);
-	data->cursor += 1;
-}
-*/
 
 void	ft_infile_to_in(char **argv, char **cmd_args, char **validpaths, int i)
 {
@@ -232,20 +182,11 @@ void	ft_infile_to_in(char **argv, char **cmd_args, char **validpaths, int i)
 			close(fd);
 		}
 		else
-		{
-			ft_double_fullfree(validpaths, cmd_args);
-			ft_close_stdfds();
-			exit(1);
-		}
+			ft_clean_exit(validpaths, cmd_args);
 	}
 	else if (i == 1 && argv[1] && access(argv[1], F_OK))
-	{
-		ft_double_fullfree(validpaths, cmd_args);
-		ft_close_stdfds();
-		exit(1);
-	}
+		ft_clean_exit(validpaths, cmd_args);
 }
-
 
 char	*ft_get_goodpath(char **cmd, char **fullpaths)
 {
@@ -267,7 +208,6 @@ char	*ft_get_goodpath(char **cmd, char **fullpaths)
 	}
 	return (clone);
 }
-
 
 char	**ft_get_fullpaths(char **paths, char *cmd)
 {
@@ -300,11 +240,13 @@ int	ft_infile_permission(char *filename)
 	return (1);
 }
 
-
 int	ft_test_files(char **argv, int ac)
 {
-	if (ft_such_file(argv[1]))
-		ft_infile_permission(argv[1]);
+	if (ac >= 5 && ft_strncmp(argv[1], "here_doc", 9))
+	{
+		if (ft_such_file(argv[1]))
+			ft_infile_permission(argv[1]);
+	}
 	if (!ft_outfile_permission(argv[ac - 1]))
 		return (0);
 	return (1);
@@ -334,24 +276,37 @@ char	**ft_get_validpaths(int ac, char **argv, char **paths)
 	return (validpaths);
 }
 
-/*
-char	*ft_get_validpath_b(t_data *data)
+char	**ft_get_paths(char **env)
 {
-	int	i;
+	int		i;
+	int		len;
+	char	**tmp;
+	char	**paths;
 
 	i = 0;
-	if (!access(data->cmd[0], F_OK | X_OK))
-		return (data->cmd[0]);
-	while (data->fullpaths && data->fullpaths[i] && access(data->fullpaths[i], \
-		F_OK | X_OK))
+	while (env && env[i] && ft_strncmp(env[i], "PATH=", 5))
 		i++;
-	if (data->fullpaths && data->fullpaths[i])
-		return (data->fullpaths[i]);
-	else
-		ft_cmd_not_found(data->cmd[0]);
-	return (NULL);
+	tmp = ft_split(env[i] + 5, ':');
+	len = ft_count_strs(env[i], ':');
+	paths = (char **)malloc(sizeof(char *) * (len + 1));
+	paths[len] = NULL;
+	i = 0;
+	while (tmp[i])
+	{
+		paths[i] = ft_strjoin(tmp[i], "/");
+		i++;
+	}
+	ft_full_free((void **)tmp);
+	tmp = NULL;
+	return (paths);
 }
-*/
+
+void	ft_clean_connect(int std, int toconnect, int toclose)
+{
+	dup2(toconnect, std);
+	close(toconnect);
+	close(toclose);
+}
 
 int	ft_check_tmpname(char *tmpname)
 {
@@ -386,62 +341,78 @@ char	*ft_get_tmpname(char **basename)
 	return (tmpname);
 }
 
-
-char	**ft_get_paths(char **env)
+int	ft_heredoc_to_file(char **infile, char *limiter)
 {
-	int		i;
-	int		len;
-	char	**tmp;
-	char	**paths;
+	int		fd;
+	int		stop;
+	char	*buffer;
+
+	stop = 0;
+	buffer = NULL;
+	if (*infile && access(*infile, F_OK) == 0)
+		*infile = ft_get_tmpname(infile);
+	fd = open(*infile, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | \
+		S_IWGRP | S_IROTH);
+	while (!stop)
+	{
+		buffer = get_next_line(STDIN);
+		if (!ft_strncmp(buffer, limiter, ft_strlen(limiter)))
+			stop++;
+		else if (buffer)
+			write(fd, buffer, ft_strlen(buffer));
+		if (buffer)
+			ft_true_free((void **)&buffer);
+	}
+	buffer = get_next_line(1024 + (STDIN + 1));
+	ft_true_free((void **)&limiter);
+	return (fd);
+}
+
+char	*ft_setup_limiter(const char *src)
+{
+	size_t	i;
+	size_t	size;
+	char	*dst;
 
 	i = 0;
-	while (env && env[i] && ft_strncmp(env[i], "PATH=", 5))
-		i++;
-	tmp = ft_split(env[i] + 5, ':');
-	len = ft_count_strs(env[i], ':');
-	paths = (char **)malloc(sizeof(char *) * (len + 1));
-	paths[len] = NULL;
-	i = 0;
-	while (tmp[i])
+	size = ft_strlen(src);
+	dst = ft_calloc(size + 2, sizeof(char));
+	while (src[i] && i < (size))
 	{
-		paths[i] = ft_strjoin(tmp[i], "/");
+		dst[i] = src[i];
 		i++;
 	}
-	ft_full_free((void **)tmp);
-	tmp = NULL;
-	return (paths);
+	dst[i] = '\n';
+	dst[i + 1] = '\0';
+	return (dst);
 }
 
-/*
-void	ft_get_next_cmd_b(t_data *data)
+char	*ft_heredoc_to_in(char **argv, char **cmd_args, char **validpaths, int i)
 {
-	data->cmd = ft_parse_cmd(data->argv[data->cursor]);
-	if (*data->cmd)
+	int		fd;
+	char	*limiter;
+	char	*infile;
+
+	if (i == 1)
 	{
-		data->paths = ft_get_paths(data->env);
-		data->fullpaths = ft_get_fullpaths(data->paths, data->cmd[0]);
-		data->validpath = ft_get_validpath_b(data);
+		infile = ft_strdup("tmp_pipex");
+		limiter = ft_setup_limiter(argv[2]);
+		fd = ft_heredoc_to_file(&infile, limiter);
+		if (fd > 0)
+		{
+			close(fd);
+			fd = open(infile, O_RDONLY);
+			if (fd < 0)
+				ft_clean_exit(validpaths, cmd_args);
+			dup2(fd, STDIN);
+			close(fd);
+			return (infile);
+		}
+		else
+			ft_clean_exit(validpaths, cmd_args);
 	}
-	else
-	{
-		ft_cmd_not_found("");
-		data->paths = ft_get_paths(data->env);
-		data->fullpaths = ft_get_fullpaths(data->paths, data->cmd[0]);
-		data->validpath = NULL;
-	}
-	data->cursor += 1;
+	return (NULL);
 }
-*/
-
-
-void	ft_clean_connect(int std, int toconnect, int toclose)
-{
-	dup2(toconnect, std);
-	close(toconnect);
-	close(toclose);
-}
-
-
 
 void	ft_pipex(t_data *data, char **validpaths)
 {
@@ -471,24 +442,68 @@ void	ft_pipex(t_data *data, char **validpaths)
 		}
 	}
 }
-/*
-int	main_bonus(t_data *data)
+
+void	ft_parent_hdoc(t_data *data, char **cmd_args, char **validpaths, int i)
+{
+	cmd_args = ft_split(data->argv[i + 3], ' ');
+	ft_append_to_outfile(validpaths, i, cmd_args, data);
+}
+
+void	ft_unlink(char *filename)
+{
+	if (filename)
+		unlink(filename);
+}
+
+char	*ft_pipex_heredoc(t_data *data, char **validpaths, char *infile)
+{
+	int			pfds[2];
+	char		**cmd_args;
+	static int	i;
+
+	i++;
+	cmd_args = ft_split(data->argv[i + 2], ' ');
+	pipe(pfds);
+	if (validpaths[i - 1] && validpaths[i - 1][0] != '\0' && !fork())
+	{
+		ft_clean_connect(STDOUT, pfds[WRITE_END], pfds[READ_END]);
+		if (!infile)
+			infile = ft_heredoc_to_in(data->argv, cmd_args, validpaths, i);
+		ft_unlink(infile);
+		execve(validpaths[i - 1], cmd_args, data->env);
+	}
+	else
+	{
+		ft_clean_connect(STDIN, pfds[READ_END], pfds[WRITE_END]);
+		ft_full_free((void **)cmd_args);
+		if ((i + 1) < (data->ac - 4))
+			ft_pipex_heredoc(data, validpaths, infile);
+		else
+			ft_parent_hdoc(data, cmd_args, validpaths, i);
+	}
+	return (infile);
+}
+
+int	main_bonus(t_data *data)// regler la gestion cpu lorsque le pipe est broken
+//ET AUSSI les fichiers tmp créés mais pas détruit
 {
 	char	**paths;
 	char	**validpaths;
+	char	*infile;
 
+	infile = NULL;
 	paths = ft_get_paths(data->env);
-	validpaths = ft_get_validpaths(data->ac, data->argv, paths);
+	validpaths = ft_get_validpaths(data->ac - 1, data->argv + 1, paths);
 	ft_full_free((void **)paths);
-	//if (ft_test_files(data->argv, data->ac))
-	//	ft_pipex(&data, validpaths);
-	//ft_print_cmderr(validpaths, data->argv);
-	//waitpid(-1, NULL, 0);
-	//ft_full_free((void **)validpaths);
+	if (ft_test_files(data->argv, data->ac))
+		infile = ft_pipex_heredoc(data, validpaths, infile);
+	ft_print_cmderr(validpaths, data->argv);
+	waitpid(-1, NULL, 0);
+	ft_full_free((void **)validpaths);
 	ft_close_stdfds();
 	exit(1);
 }
-*/
+
 int	main(int ac, char **argv, char **env)
 {
 	t_data	data;
@@ -496,7 +511,9 @@ int	main(int ac, char **argv, char **env)
 	char	**validpaths;
 
 	data = ft_init_data(ac, argv, env);
-	if (ac >= 5 && ft_strncmp(argv[1], "here_doc", 9))
+	if (ac >= 6 && !ft_strncmp(argv[1], "here_doc", 9))
+		main_bonus(&data);
+	else if (ac >= 5 && ft_strncmp(argv[1], "here_doc", 9))
 	{
 		paths = ft_get_paths(env);
 		validpaths = ft_get_validpaths(ac, argv, paths);
@@ -507,8 +524,6 @@ int	main(int ac, char **argv, char **env)
 		waitpid(-1, NULL, 0);
 		ft_full_free((void **)validpaths);
 	}
-	//else if (ac >= 6 && !ft_strncmp(argv[1], "here_doc", 9))
-	//	main_bonus(&data);
 	else
 		write(2, "ERROR : wrong number of arguments.\n", 35);
 	ft_close_stdfds();
